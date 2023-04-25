516396ae2f489e891e2b3604137fcc11762f2914
justgraceg <uniquelyu23@gmail.com>
2023-04-25 12:13:13 +0100

    ingredients!!

--- ingredients/index.js
+++ ingredients/index.js
@@ -1,7 +1,39 @@
-function on(id) {
-    let ingredient = id.getAttribute("ingredient");
-    console.log(ingredient);
+template = '<h6>FULLNAME</h6><button onClick="on(this)" ingredient="INGREID"><img src="../foodsprites/INGREID.png" alt="FULLNAME"></button>'
 
+var node;
+var textnode;
+
+for (var greed of Object.keys(INGREDIENTS)) {
+
+    text = template.replace("FULLNAME", INGREDIENTS[greed]["Name"]).replace("INGREID", greed);
+    text = text.replace("FULLNAME", INGREDIENTS[greed]["Name"]).replace("INGREID", greed);
+
+    node = document.createElement("li");
+    node.setAttribute("id", greed);
+    node.setAttribute("class", "ingredient");
+    document.getElementById("ingrelist").appendChild(node);
+
+    document.getElementById(greed).innerHTML = text;
+}
+
+
+
+
+function on(id) {
+
+    let ingredient = id.getAttribute("ingredient");
+
+    // set title
+    document.getElementById("title").innerHTML = INGREDIENTS[ingredient]["Name"];
+
+    // set description
+    if (Object.keys(INGREDIENTS[ingredient]).includes("Effect")) {
+        document.getElementById("desc").innerHTML = INGREDIENTS[ingredient]["Effect"];
+    } else {
+        document.getElementById("desc").innerHTML = "No Effect."
+    }
+    
+    // make overlay visible
     document.getElementById("overlay").style.display = "block";
 
 }
